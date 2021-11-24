0e15185ca787ccd33784995a98ebdf8b02ceba54
Airton <ajcolombini@gmail.com>
2021-11-24 16:06:45 -0300

    Initial commit

---
+++ BlazorApp/Data/WeatherForecastService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+   
+            "Congelante", "Fresco", "Frio", "Gelado", "Suave", "Calor", "Ameno", "Quente", "Sufocante", "Ardente"
+        };
+
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
